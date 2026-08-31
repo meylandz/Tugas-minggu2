491029edb6708181d04ff9d3a2bc5c26d82dc427
meylandz <e41250117@student.polije.ac.id>
2026-08-31 10:03:10 +0700

    pembaruan  file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/biodata', function () {
+    return view('biodata');
+});
+
+Route::get('/pendidikan', function () {
+    return view('pendidikan');
+});
+
+Route::get('/prestasi', function () {
+    return view('prestasi');
+});
